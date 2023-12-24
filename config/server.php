29d28bae0e634c3edffee6c7df1f04ee6c9b64bd
hongfs <hong@hongfs.cn>
2023-12-24 20:55:12 +0800

    Update server.php

--- config/server.php
+++ config/server.php
@@ -17,7 +17,7 @@ return [
     'transport' => 'tcp',
     'context' => [],
     'name' => 'webman',
-    'count' => cpu_count() * 4,
+    'count' => 20,
     'user' => '',
     'group' => '',
     'reusePort' => false,
